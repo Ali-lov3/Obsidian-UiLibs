30c36eb67e1b75f7eeb311096c09e90f347756ff
Ali <mf.law123@gmail.com>
2026-06-13 13:12:52 +0200

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,7 +1,7 @@
 local repo = "https://raw.githubusercontent.com/Ali-lov3/Obsidian-UiLibs/refs/heads/main/"
 local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
-local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ali-lov3/Obsidian-UiLibs/main/addons/ThemeManager.lua.txt"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ali-lov3/Obsidian-UiLibs/main/addons/SaveManager.lua.txt"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ali-lov3/Obsidian-UiLibs/refs/heads/main/addons/ThemeManager.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ali-lov3/Obsidian-UiLibs/refs/heads/main/addons/SaveManager.lua"))()
 local Options = Library.Options
 local Toggles = Library.Toggles
 Library.ForceCheckbox = false
